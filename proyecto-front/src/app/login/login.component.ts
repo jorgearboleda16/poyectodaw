de5e6ce57c2f2debb04f5f9c9f59e20a15914430
Jorge Arboleda <jarboleda@justcapital.media>
2024-06-05 22:19:12 -0500

    SIRVIO al fin el front

--- proyecto-front/src/app/login/login.component.ts
+++ proyecto-front/src/app/login/login.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [CommonModule, ReactiveFormsModule] // Asegúrate de importar los módulos necesarios
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Realiza la lógica de autenticación aquí
+      console.log(this.loginForm.value);
+    }
+  }
+}
