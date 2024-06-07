5e6e8cc652a81483abad8fb405f4c5930cfa8670
Jorge Arboleda <jarboleda@justcapital.media>
2024-06-07 18:34:31 -0500

    Ya sirve el menu

--- proyecto-front/src/app/app.component.ts
+++ proyecto-front/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ConocenosComponent } from './conocenos/conocenos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
+import { ReservacionesComponent } from './reservaciones/reservaciones.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ConocenosComponent,HomeComponent,LoginComponent,MenuComponent,RegisterComponent,ReservacionesComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
